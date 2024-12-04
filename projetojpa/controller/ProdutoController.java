cdf24d0810c4e5ef4eed5bb90506e379d95e9ddc
majucorrea07 <159278297+majucorrea07@users.noreply.github.com>
2024-12-04 15:37:09 -0300

    a

---
+++ projetojpa/controller/ProdutoController.java
@@ -0,0 +1,46 @@
+package com.projetojpa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetojpa.entities.Produto;
+import com.projetojpa.services.ProdutoService;
+
+@RestController
+@RequestMapping("/produto")
+
+public class ProdutoController {
+	private final ProdutoService produtoService;
+
+	@Autowired 
+	public ProdutoController(ProdutoService produtoService) {
+		this.produtoService = produtoService;
+	}
+
+	@PostMapping
+	public Produto createProduct(@RequestBody Produto produto) {
+		return produtoService.saveProduto(produto);
+	}
+
+	@PostMapping ("/(id)")
+	public Produto getProduto(@PathVariable Long id) {
+		return produtoService.getProdutoById(id);
+	}
+
+	@PostMapping
+	public List<Produto> getAllProdutos() {
+		return produtoService.getAllProdutos();
+	}
+
+	@DeleteMapping("/(id)")
+	public void deleteProduto(@PathVariable Long id) {
+		produtoService.deleteProduto(id);
+	}
+}
---
+++ projetojpa/entities/Produto.java
@@ -0,0 +1,48 @@
+package com.projetojpa.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Produto")
+
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome")
+	private String name;
+    
+	@Column(name = "preco")
+    private double price;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
